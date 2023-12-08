027a2c787c3972a04c6be061955b0027345b582e
2451134 <bds6052@gmail.com>
2023-12-08 09:18:00 +0000

    Refactor messages in server code

--- rtSwitchboard/server.js
+++ rtSwitchboard/server.js
@@ -1,15 +1,13 @@
 import express from "express"
 import nodeCron from "node-cron"
-import pkg from "body-parser"
+import bodyParser from "body-parser"
 import Producer from "./producer.js"
-
-const { json } = pkg
 
 const producer = new Producer()
 
 const app = express()
 
-app.use(json("application/json"))
+app.use(bodyParser.json())
 
 // app.post("/sendLog", async (req, res, next) => {
 //   await producer.publishMessage(req.body.logType, req.body.message)
@@ -18,8 +16,11 @@ app.use(json("application/json"))
 
 nodeCron.schedule("*/5 * * * * *", () => {
   // Do whatever you want in here. Send email, Make  database backup or download data.
-  producer.publishMessage("Weather", "This is a Weather message")
-  producer.publishMessage("News", "This is a News message")
+  const weatherMessage = "This is a Weather message"
+  const newsMessage = "This is a News message"
+
+  producer.publishMessage("Weather", weatherMessage)
+  producer.publishMessage("News", newsMessage)
 })
 
 app.listen(3000, () => {
